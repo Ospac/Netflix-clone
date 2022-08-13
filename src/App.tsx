3239c98a1ce463c7467b5ed91d7ead9e35b042e4
geha <ospac111@gmail.com>
2022-08-13 17:08:57 +0900

    setup & header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from 'routes/Home';
+import Tv from 'routes/Tv';
+import Search from 'routes/Search';
+import Header from 'components/Header';
+import { createGlobalStyle } from 'styled-components';
+import Movie from 'routes/Movie';
+const GlobalStyles = createGlobalStyle`
+    a {color: #fff; text-decoration: none; outline: none}
+    a:hover, a:active {text-decoration: none; color:#fff;}
+    body {
+        background-color: white;
+    }
+    button {
+      background : none;
+      border : 0;
+    }
+    input{
+      &:focus{
+        outline: 0;
+      }
+    }
 
+`;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+    <GlobalStyles/>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/tv" element={<Tv/>}></Route>
+        <Route path="/movie" element={<Movie/>}></Route>
+        <Route path="/search" element={<Search/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  </>
 }
 
 export default App;
---
+++ src/routes/Home.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export default function Home (){
+    return <Container></Container>
+}
+const Container = styled.div`
+    background-color: #ffffff;
+    height: 200vh;
+`
